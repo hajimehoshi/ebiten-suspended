6477a54386975c238d11f172a6a7fa5e35450ca6
Hajime Hoshi <hajimehoshi@gmail.com>
2012-01-08 02:53:39 -0800

    Miss spelling... thrid -> third...

---
+++ samples/Ebiten-iOS/Ebiten-iOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Ebiten-iOS
+//
+//  Created by Hajime Hoshi on 1/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
