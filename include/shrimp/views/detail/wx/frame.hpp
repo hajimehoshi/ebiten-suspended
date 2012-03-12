d3bf5b00e8533b0e54b73f35ddb476939055b53d
Hajime Hoshi <hajimehoshi@gmail.com>
2012-03-13 02:10:44 +0900

    Modified frame

--- include/shrimp/views/detail/wx/frame.hpp
+++ include/shrimp/views/detail/wx/frame.hpp
@@ -4,6 +4,7 @@
 #include "shrimp/views/detail/wx/gl_canvas.hpp"
 #include "shrimp/views/detail/wx/wx.hpp"
 #include "ebiten/ebiten.hpp"
+#include <wx/splitter.h>
 #include <functional>
 #include <memory>
 
@@ -25,19 +26,18 @@ private:
          ebiten::graphics::texture& main_offscreen) {
       g.set_offscreen(main_offscreen);
       g.clear();
-      g.draw_rect(0, 50, 100, 100,
+      g.draw_rect(0, 100, 50, 50,
                   0, 128, 255, 255);
     }
   };
   std::unique_ptr<map_editor_drawer> map_editor_drawer_;
-private:
-  gl_canvas* map_editor_;
 public:
   frame()
     : wxFrame(nullptr,
               wxID_ANY,
-              wxT("Shrimp")),
-      map_editor_(nullptr) {
+              wxT("Shrimp"),
+              wxDefaultPosition,
+              wxSize(800, 600)) {
     {
       wxMenu* file_menu = new wxMenu();
       file_menu->Append(wxID_EXIT, wxT("E&xit\tALT-X"));
@@ -51,15 +51,53 @@ public:
       // TODO: Modify tool_bar
       tool_bar->Realize();
     }
-    auto update = std::bind(&map_editor_drawer::update,
-                            this->map_editor_drawer_.get(),
-                            std::placeholders::_1,
-                            std::placeholders::_2);
-    auto draw = std::bind(&map_editor_drawer::draw,
-                          this->map_editor_drawer_.get(),
-                          std::placeholders::_1,
-                          std::placeholders::_2);
-    this->map_editor_ = new gl_canvas(this, update, draw);
+    auto main_splitter = new wxSplitterWindow(this,
+                                              wxID_ANY,
+                                              wxDefaultPosition,
+                                              wxDefaultSize,
+                                              wxSP_LIVE_UPDATE | wxSP_NOBORDER);
+    {
+      auto source_list_panel = new wxPanel(main_splitter);
+      auto model_panel       = new wxPanel(main_splitter);
+      //left_panel->SetBackgroundColour(wxColour(0xc6, 0xd0, 0xda));
+      main_splitter->SplitVertically(source_list_panel,
+                                     model_panel,
+                                     120);
+      // model
+      {
+        auto model_splitter = new wxSplitterWindow(model_panel,
+                                                   wxID_ANY,
+                                                   wxDefaultPosition,
+                                                   wxDefaultSize,
+                                                   wxSP_LIVE_UPDATE | wxSP_NOBORDER);
+        {
+          auto sizer = new wxBoxSizer(wxHORIZONTAL);
+          sizer->Add(model_splitter, 1, wxEXPAND, 0);
+          model_panel->SetSizer(sizer);
+        }
+        auto entries_panel = new wxPanel(model_splitter);
+        auto editor_panel  = new wxPanel(model_splitter);
+        model_splitter->SplitVertically(entries_panel,
+                                        editor_panel,
+                                        120);
+        // map editor
+        {
+          auto update = std::bind(&map_editor_drawer::update,
+                                  this->map_editor_drawer_.get(),
+                                  std::placeholders::_1,
+                                  std::placeholders::_2);
+          auto draw = std::bind(&map_editor_drawer::draw,
+                                this->map_editor_drawer_.get(),
+                                std::placeholders::_1,
+                                std::placeholders::_2);
+          auto c = new gl_canvas(editor_panel,
+                                 update, draw);
+          auto sizer = new wxBoxSizer(wxHORIZONTAL);
+          sizer->Add(c, 1, wxEXPAND, 0);
+          editor_panel->SetSizer(sizer);
+        }
+      }
+    }
   }
   void
   on_exit(wxCommandEvent&) {
